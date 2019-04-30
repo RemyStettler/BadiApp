95a2a5d7f7ad4e59f497fc475d4c412131253519
Gassmann Flynn <bgassf@bbc-be.local>
2019-04-30 09:30:47 +0200

    update MainActivity

--- app/src/main/java/com/example/badiappflynnremy/MainActivity.java
+++ app/src/main/java/com/example/badiappflynnremy/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.badiappflynnremy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.badiappflynnremy.dal.BadiDao;
+import com.example.badiappflynnremy.model.Badi;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Übersicht");
+    }
+
+    private void addBadisToClickableList(){
+        ListView badis = findViewById(R.id.badiliste);
+        ArrayAdapter<Badi> badiAdapter =
+                new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
+        badiAdapter.addAll(BadiDao.getAll());
+        badis.setAdapter(badiAdapter);
+
     }
 }
